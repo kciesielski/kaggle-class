a743bd482d5828cdc65a2f6a59bfb7ca0b5a7e6e
Krzysztof Ciesielski <panciesielski@gmail.com>
2012-10-30 22:02:11 +0100

    Unifying algorithms
    Unifying algorithms

---
+++ algorithms/cost.m
@@ -0,0 +1,13 @@
+function [J, grad] = cost(theta, X, y, lambda)
+% Initialize some useful values
+m = length(y); % number of training examples
+
+J = 0;
+grad = zeros(size(theta));
+lambdaByM =  lambda / m;
+lambdaBy2M =  lambdaByM / 2;
+hypoX = sigmoid(X*theta);
+J = (sum(-y .* log(hypoX) - ((1 - y) .* log(1 - hypoX))) / m) + (lambdaBy2M * sum(theta(2:size(theta,1)) .^ 2));
+grad = (((hypoX - y)' * X) / m) + (theta' * lambdaByM);
+grad(1) = ((hypoX - y)' * X(:,1)) / m;
+end
